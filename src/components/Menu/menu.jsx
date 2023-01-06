5b79b856cb0b0857608a38f4f04069c6dba85fb5
Alex Koniushenko <alex.koniushenko@gmail.com>
2023-01-06 15:56:46 +0200

    menu component

---
+++ src/components/Menu/menu.jsx
@@ -0,0 +1,14 @@
+export const Menu = () => {
+  return (
+    <section>
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Movies</a>
+        </li>
+      </ul>
+    </section>
+  );
+};
